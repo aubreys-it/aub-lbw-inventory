46f5f2d83e53a13435d0c05fe8bfe1e1820bbded
Aubrey's IT <95295945+aubreys-it@users.noreply.github.com>
2025-09-25 13:37:07 +0000

    Load App Into Azure

--- config.py
+++ config.py
@@ -5,9 +5,9 @@ SECRET_KEY = os.environ.get('')
 SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 
-#MAIL_SERVER = 'smtp.example.com'
-#MAIL_PORT = 587
-#MAIL_USE_TLS = True
-#MAIL_USERNAME = 'your-email@example.com'
-#MAIL_PASSWORD = 'your-email-password'
-#MAIL_DEFAULT_SENDER = 'your-email@example.com'
+MAIL_SERVER = 'smtp.example.com'
+MAIL_PORT = 587
+MAIL_USE_TLS = True
+MAIL_USERNAME = 'your-email@example.com'
+MAIL_PASSWORD = 'your-email-password'
+MAIL_DEFAULT_SENDER = 'your-email@example.com'
